feb0b5de76dcb4a4539f6044b8084e33eeb0a92a
ashishkunthe <ashishkunthe2003@gmail.com>
2024-08-27 20:22:41 +0530

    new app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
 
-function App() {
-  return <div className="App"></div>;
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 1 };
+    this.handleDecrement = this.handleDecrement.bind(this);
+  }
+  handleDecrement() {
+    this.setState = this.count - 1;
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={() => this.handleDecrement()}>-</button>
+        <span>{this.state.count}</span>
+        <button>+</button>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Counter;
